6053d5a929d0372273041a3ac6b1c91fb8985989
Daniyar <daniyar.elebesov@gmail.com>
2025-07-03 09:52:49 +0600

    Monday

--- src/main/java/com/autobots/git/Add.java
+++ src/main/java/com/autobots/git/Add.java
@@ -3,5 +3,17 @@ package com.autobots.git;
 public class Add {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        // 1. git
+        // 2.
+        // 3.
+        // 4. local storage
+        // 5. remote storage
+        // 6.
+        // 7.
+        // 8. 1. Working directory
+        // 9. 2. staging area/
+        // 10.
+
     }
 }
