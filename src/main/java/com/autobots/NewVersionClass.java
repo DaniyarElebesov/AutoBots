93b70200ab5e7fed2236e755cd24f814b6988046
Daniyar <daniyar.elebesov@gmail.com>
2025-07-07 11:41:46 +0600

    Merge remote-tracking branch 'origin/nursultan/new-version-4.0.0' into nursultan/new-version-4.0.0

--- src/main/java/com/autobots/NewVersionClass.java
+++ src/main/java/com/autobots/NewVersionClass.java
@@ -3,5 +3,6 @@ package com.autobots;
 public class NewVersionClass {
     public static void main(String[] args) {
         System.out.println("New version 4.0.0");
+        System.out.println("New version 4.0.1");
     }
 }
